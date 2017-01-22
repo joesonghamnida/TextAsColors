078527900b3087fde5f7ecf28507f928cb709240
Joe Morris <joe.morris.dev@gmail.com>
2017-01-22 15:38:00 -0600

    set up controllers

--- src/main/java/colors/controllers/DisplayGetController.java
+++ src/main/java/colors/controllers/DisplayGetController.java
@@ -1,7 +1,26 @@
 package colors.controllers;
 
-/**
- * Created by joe on 1/21/17.
- */
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
+import java.io.FileNotFoundException;
+
+@Controller
 public class DisplayGetController {
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String home() {
+        return ("home");
+    }
+
+    @RequestMapping(path = "/displayResults", method = RequestMethod.GET)
+    public String displayResults(HttpSession session, Model model) throws FileNotFoundException {
+        String rawText = (String) session.getAttribute("rawText");
+
+        //model.addAttribute("results",results);
+        return ("home");
+    }
 }
--- src/main/java/colors/controllers/PastePostController.java
+++ src/main/java/colors/controllers/PastePostController.java
@@ -1,7 +1,24 @@
 package colors.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
+
 /**
  * Created by joe on 1/21/17.
  */
+
+@Controller
 public class PastePostController {
+
+    @RequestMapping(path = "/paste", method = RequestMethod.POST)
+    public String pasteText(HttpSession session, String document) {
+        session.setAttribute("rawText", document);
+
+        return "redirect:/displayResults";
+    }
 }
+
+
