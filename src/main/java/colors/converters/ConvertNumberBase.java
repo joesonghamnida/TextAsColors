fad6adaf36e42d471579debb381aa73e774c00ec
Joe Morris <joe.morris.dev@gmail.com>
2017-01-24 17:15:27 -0600

    fleshed out binary conversion

--- src/main/java/colors/converters/ConvertNumberBase.java
+++ src/main/java/colors/converters/ConvertNumberBase.java
@@ -5,8 +5,31 @@ package colors.converters;
  */
 public class ConvertNumberBase {
 
-    public static void convertToBinary(){
+    //using ascii
+    public static String convertToBinary(int value){
+        String binary = "";
+        int denominator = 128;
+        while(denominator > 0){
+            int remainder = value % (denominator);
+            int placeHolder = remainder;
 
+            remainder = checkRemainder(remainder, value);
+            binary += String.valueOf(remainder);
+
+            value = placeHolder;
+            denominator = denominator / 2;
+        }
+        return binary;
+    }
+
+    public static int checkRemainder(int remainder, int value){
+        if(remainder == value){
+            remainder = 0;
+        }
+        else{
+            remainder = 1;
+        }
+        return remainder;
     }
 
     public static void convertToOctal(){
