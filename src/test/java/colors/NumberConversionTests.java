79ae780ce036800c14ad7e42d90a7c3753b1b474
Joe Morris <joe.morris.dev@gmail.com>
2017-01-24 13:44:03 -0600

    test outline initial commit

---
+++ src/test/java/colors/NumberConversionTests.java
@@ -0,0 +1,24 @@
+package colors;
+
+import org.junit.Test;
+
+/**
+ * Created by joe on 1/24/17.
+ */
+public class NumberConversionTests {
+
+    @Test
+    public void convertBytesToBinary(){
+
+    }
+
+    @Test
+    public void convertBytesToOctal(){
+
+    }
+
+    @Test
+    public void convertBytesToHex(){
+
+    }
+}
