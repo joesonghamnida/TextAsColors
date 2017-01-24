c245744947b15628242f111dd693d6bc26f4762b
Joe Morris <joe.morris.dev@gmail.com>
2017-01-24 17:14:40 -0600

    tested binary conversion

--- src/test/java/colors/NumberConversionTests.java
+++ src/test/java/colors/NumberConversionTests.java
@@ -1,6 +1,11 @@
 package colors;
+import colors.converters.*;
+import colors.Word;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 /**
  * Created by joe on 1/24/17.
@@ -8,8 +13,17 @@ import org.junit.Test;
 public class NumberConversionTests {
 
     @Test
+    //using ascii
     public void convertBytesToBinary(){
+        String testString = "a";
+        int testStringValue = 0;
+        ArrayList<Word> words = TextToByte.textToByte(testString);
+        for(Word word : words){
+            testStringValue = word.getValue();
+        }
 
+        Assert.assertTrue("01100001".equals(ConvertNumberBase.convertToBinary(testStringValue)));
+        Assert.assertTrue("01100010".equals(ConvertNumberBase.convertToBinary(98)));
     }
 
     @Test
